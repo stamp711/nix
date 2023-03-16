27674741401e3f11b52b1830abffb10499659922
stamp711 <stamp1024@gmail.com>
2023-03-17 00:30:54 +0800

    Add ls flake

--- flake.nix
+++ flake.nix
@@ -10,14 +10,18 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    # ~bwolf's language-servers.nix
+    language-servers.url = git+https://git.sr.ht/~bwolf/language-servers.nix;
+    language-servers.inputs.nixpkgs.follows = "nixpkgs";
+
     nix-colors.url = "github:misterio77/nix-colors";
   };
 
-  outputs = { nixpkgs, home-manager, nix-colors, ... }@inputs: rec {
+  outputs = { nixpkgs, home-manager, nix-colors, language-servers, ... }@inputs: rec {
 
     homeConfigurations."stamp@darwin" = home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages."x86_64-darwin";
-      extraSpecialArgs = { inherit nix-colors; };
+      extraSpecialArgs = { inherit nix-colors language-servers; };
       modules = [
         {
           home.username = "stamp";
@@ -29,7 +33,7 @@
 
     homeConfigurations."stamp@linux" = home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages."x86_64-linux";
-      extraSpecialArgs = { inherit nix-colors; };
+      extraSpecialArgs = { inherit nix-colors language-servers; };
       modules = [
         {
           home.username = "stamp";
@@ -41,7 +45,7 @@
 
     homeConfigurations."liuxuyang@linux" = home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages."x86_64-linux";
-      extraSpecialArgs = { inherit nix-colors; };
+      extraSpecialArgs = { inherit nix-colors language-servers; };
       modules = [
         {
           home.username = "liuxuyang";
