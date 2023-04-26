8f53bec1c52c3c50070707abef6cf6d09b705dfa
stamp711 <stamp1024@gmail.com>
2023-04-26 16:26:16 +0800

    Change git user.name

--- home.nix
+++ home.nix
@@ -211,7 +211,7 @@ in {
   programs.bat.enable = true;
 
   programs.git.enable = true;
-  programs.git.userName = "stamp711";
+  programs.git.userName = "Apricity";
   programs.git.userEmail = "stamp1024@gmail.com";
   programs.git.signing.key =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG0Zuk/bYRvsX5WypXgY7aopBeoTNjma1rr6Txtp87JS";
