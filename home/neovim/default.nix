56bccb4ef5d9ef708a462971c403ff4795d5f6ff
Apricity <stamp1024@gmail.com>
2023-05-06 23:02:35 +0800

    wip: neovim

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -1,4 +1,6 @@
 {...}: {
+  programs.neovim.enable = true;
+  programs.neovim.defaultEditor = true;
   xdg.configFile."nvim/init.lua".source = ./nvim/init.lua;
   xdg.configFile."nvim/lua".source = ./nvim/lua;
 }
