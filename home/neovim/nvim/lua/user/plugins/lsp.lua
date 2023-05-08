fd3d2736d7343ec123794ecb0a2ce109e8e79d5d
stamp711 <stamp1024@gmail.com>
2023-05-08 15:34:48 +0800

    wip: neovim

--- home/neovim/nvim/lua/user/plugins/lsp.lua
+++ home/neovim/nvim/lua/user/plugins/lsp.lua
@@ -1,25 +1,20 @@
 return {
-  { "williamboman/mason.nvim",           cmd = "Mason",                            config = true },
-  { "williamboman/mason-lspconfig.nvim", dependencies = "williamboman/mason.nvim", config = true },
+  { "williamboman/mason.nvim", cmd = "Mason", config = true },
   {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
-    dependencies = { "williamboman/mason-lspconfig.nvim" },
+    dependencies = { "mason.nvim", "williamboman/mason-lspconfig.nvim", "simrat39/rust-tools.nvim" },
     -- TODO: write the setup function which takes opts merged from different files (for different languages)
+    -- refer: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/extras/lang/typescript.lua
     opts = {},
     config = function(_, opts)
       local mlsp = require("mason-lspconfig");
-      mlsp.setup();
+      mlsp.setup { automatic_installation = true };
       mlsp.setup_handlers {
-        -- The first entry (without a key) will be the default handler
-        -- and will be called for each installed server that doesn't have
-        -- a dedicated handler.
         function(server_name) -- default handler (optional)
           require("lspconfig")[server_name].setup {}
         end,
-        -- Next, you can provide a dedicated handler for specific servers.
-        -- For example, a handler override for the `rust_analyzer`:
-        ["rust_analyzer"] = function()
+        ["rust_analyzer"] = function() -- override for `rust_analyzer`
           require("rust-tools").setup {}
         end
       }
