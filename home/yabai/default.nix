0be3037b23dbb7d007d8b0f7e948bfdae0075afe
Apricity <stamp1024@gmail.com>
2023-06-07 17:37:55 +0800

    remove yabai

--- home/yabai/default.nix
+++
@@ -1,8 +0,0 @@
-{config, ...}: let
-  inherit (config.home.user-info) nixConfigDirectory;
-  pwd = "${nixConfigDirectory}/home/yabai";
-  symlink = (config.lib.file).mkOutOfStoreSymlink;
-in {
-  xdg.configFile."yabai".source = symlink "${pwd}/yabai";
-  xdg.configFile."skhd".source = symlink "${pwd}/skhd";
-}
