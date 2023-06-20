a84bb66d0216ece5dfbe8bbc687927d30190ea55
Apricity <stamp1024@gmail.com>
2023-06-20 18:33:46 +0800

    add kitty config

---
+++ home/kitty.nix
@@ -0,0 +1,15 @@
+{ pkgs, ...}: {
+  programs.kitty.enable = true;
+  programs.kitty.font = {
+    package = pkgs.nerdfonts.override { fonts = [ "UbuntuMono" ]; };
+    name = "UbuntuMono Nerd Font";
+    size = 15;
+  };
+  programs.kitty.shellIntegration.mode = "no-cursor";
+  programs.kitty.settings = {
+    cursor_blink_interval = 0;
+    initial_window_width = "150c";
+    initial_window_height = "45c";
+    background_opacity = "0.8";
+  };
+}
