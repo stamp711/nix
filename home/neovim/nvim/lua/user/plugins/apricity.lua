5d964d228ec49aa8fabf8693289dd23606888c88
Apricity <stamp1024@gmail.com>
2023-06-21 18:04:44 +0800

    neovim: enable trailblazer auto session save & load

--- home/neovim/nvim/lua/user/plugins/apricity.lua
+++ home/neovim/nvim/lua/user/plugins/apricity.lua
@@ -113,7 +113,10 @@ return {
 
   {
     "LeonHeidelbach/trailblazer.nvim",
-    opts = {},
+    opts = {
+      auto_save_trailblazer_state_on_exit = true,
+      auto_load_trailblazer_state_on_enter = true,
+    },
   },
 
   -- Telescope & related
