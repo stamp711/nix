1b8aa6b00e64053716f57a3c55bbc4959489abd6
Apricity <stamp1024@gmail.com>
2023-06-26 15:30:38 +0800

    neovide

--- home/neovim/nvim/lua/user/neovide.lua
+++ home/neovim/nvim/lua/user/neovide.lua
@@ -1,4 +1,5 @@
 if vim.g.neovide then
-	local opt = vim.opt
-	opt.guifont = "Hack Nerd Font:16"
+  local opt = vim.opt
+  opt.guifont = "UbuntuMono Nerd Font:16"
+  vim.g.neovide_transparency = 0.7
 end
