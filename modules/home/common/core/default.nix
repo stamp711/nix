381a6bba3a6fcc78802f8a0f58eafbd98ffc4adb
liuxuyang.plt <liuxuyang.plt@bytedance.com>
2026-02-11 14:54:19 +0800

    nixpkgs allowUnfree

--- modules/home/common/core/default.nix
+++ modules/home/common/core/default.nix
@@ -22,5 +22,8 @@
         "nix-command"
         "flakes"
       ];
+
+      nixpkgs.config = import ./nixpkgs-config.nix;
+      xdg.configFile."nixpkgs/config.nix".source = ./nixpkgs-config.nix;
     };
 }
