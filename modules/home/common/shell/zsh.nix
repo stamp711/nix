eb6d0c022c47efdaf198dfe8b1214cadd348f365
Apricity <stamp1024@gmail.com>
2026-02-12 15:00:24 +0800

    Zsh tweaks

--- modules/home/common/shell/zsh.nix
+++ modules/home/common/shell/zsh.nix
@@ -1,5 +1,5 @@
 {
-  description = "Zsh with oh-my-zsh, starship prompt, history search, and theme";
+  description = "Zsh with oh-my-zsh, starship prompt, and modern history";
 
   module =
     {
@@ -25,30 +25,35 @@
         autosuggestion.enable = true;
         enableVteIntegration = true;
         syntaxHighlighting.enable = true;
+        history = {
+          size = 100000;
+          save = 100000;
+          expireDuplicatesFirst = true;
+          extended = true;
+        };
         oh-my-zsh = {
           enable = true;
-          theme = "robbyrussell";
+          theme = "";
           plugins = [
+            "aliases"
             # Productivity
             "command-not-found"
             "encode64"
             "extract"
             "fbterm"
-            "history-substring-search"
             "urltools"
             "web-search"
             # Build tools
             "git"
             "gitignore"
             "gnu-utils"
+            "jj"
             "kubectl"
             # Distro-related
             "systemd"
             # macOS
             "brew"
             "macos"
-            # Misc
-            "themes"
           ];
         };
         plugins = [
@@ -57,8 +62,14 @@
             src = pkgs.zsh-nix-shell;
             file = "share/zsh-nix-shell/nix-shell.plugin.zsh";
           }
+          {
+            name = "you-should-use";
+            src = pkgs.zsh-you-should-use;
+            file = "share/zsh/plugins/you-should-use/you-should-use.plugin.zsh";
+          }
         ];
         initContent = ''
+          export YSU_MESSAGE_FORMAT="💡 $(tput setab 22)$(tput setaf 231) %alias $(tput sgr0)"
           unset __HM_SESS_VARS_SOURCED # workaround for orbstack shells
           [ -f ~/.config/op/plugins.sh ] && source ~/.config/op/plugins.sh
           # Apply color scheme
@@ -69,6 +80,16 @@
       };
 
       programs.fzf.enable = true;
+
+      programs.atuin = {
+        enable = true;
+        flags = [ "--disable-up-arrow" ];
+        settings = {
+          style = "auto";
+          invert = true;
+          inline_height = 20;
+        };
+      };
 
       # Starship prompt
       home.packages = [
