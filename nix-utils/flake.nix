8965e363ee6be576708c33efe13f09b244351e45
Apricity <stamp1024@gmail.com>
2026-02-16 19:16:50 +0800

    update flakes

--- nix-utils/flake.nix
+++
@@ -1,9 +0,0 @@
-{
-  description = "Shared Nix utility functions";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  outputs =
-    { nixpkgs, ... }:
-    {
-      lib = import ./import.nix { inherit (nixpkgs) lib; };
-    };
-}
