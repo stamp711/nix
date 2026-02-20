1651ac3ed352664cea7f1f00110a16384f2a1bd8
Apricity <stamp1024@gmail.com>
2026-02-21 00:50:28 +0800

    rekey for personal MBP

--- hosts/personal/macbook.nix
+++ hosts/personal/macbook.nix
@@ -10,6 +10,11 @@ in
 
   homeConfiguration = self.lib.mkHome {
     inherit system username;
-    modules = [ self.homeProfiles.personal ];
+    modules = [
+      self.homeProfiles.personal
+      {
+        age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO1MxOTUJLMz6ehWbLVHAnhG8CR25DjmoXXUGIw3s/wN";
+      }
+    ];
   };
 }
