a8db3f2ad653c24add333b00747c66ec3a5dea1b
Apricity <stamp1024@gmail.com>
2026-02-21 01:52:59 +0800

    meta.description for update-inputs app

--- apps.nix
+++ apps.nix
@@ -35,6 +35,7 @@
 
   update-inputs = {
     type = "app";
+    meta.description = "Update nixpkgs to latest Hydra-cached revision and other inputs to newest";
     program = toString (
       pkgs.writeShellScript "update-nixpkgs" ''
         rev=$(${pkgs.curl}/bin/curl -sL https://channels.nixos.org/nixpkgs-unstable/git-revision)
