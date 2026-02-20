06fbb296afcf1619218253756c825293f110159a
Apricity <stamp1024@gmail.com>
2026-02-20 13:04:09 +0800

    mise

--- modules/home/shell/direnv.nix
+++ modules/home/shell/direnv.nix
@@ -1,10 +1,12 @@
 {
-  description = "Direnv with nix-direnv integration";
+  description = "Direnv with nix-direnv integration & mise";
 
   module = {
     programs.direnv = {
       enable = true;
       nix-direnv.enable = true;
+      mise.enable = true;
     };
+    programs.mise.enable = true;
   };
 }
