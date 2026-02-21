28955cf8b4ca5d3847ab7964393defa138d543f6
Apricity <stamp1024@gmail.com>
2026-02-21 23:49:03 +0800

    update GPD

--- hosts/personal/gpd/default.nix
+++ hosts/personal/gpd/default.nix
@@ -3,45 +3,65 @@ let
   username = "stamp";
   hostname = "GPD";
   system = "x86_64-linux";
+  hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBTyOPbsihNDeD7xK5TCqRa/68JY1ff6tp0plNjkda1s";
+  userPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGCt7+OlTiE/l4dWFibRgI2VjEAs4+veq2qw2JdNsrCt";
 in
 {
   description = "GPD Pocket 4";
   inherit username hostname system;
 
+  deploy = {
+    hostname = "GPD.home";
+  };
+
   nixosConfiguration = self.lib.mkNixos {
     inherit system;
     modules = [
       self.nixosModules.common.core
+      self.nixosModules.common.agenix-rekey
       self.nixosModules.common.audio
       self.nixosModules.common.networking
       ./hardware.nix
       ./lte.nix
       self.nixosModules.desktop.gnome
       self.nixosModules.boot-disk.efi-btrfs
-      {
-        networking.hostName = hostname;
-        boot-disk.device = "/dev/nvme0n1";
-        boot.initrd.availableKernelModules = [
-          "nvme"
-          "xhci_pci"
-          "thunderbolt"
-        ];
+      (
+        { pkgs, ... }:
+        {
+          networking.hostName = hostname;
+          age.rekey.hostPubkey = hostPubkey;
 
-        # Primary user
-        users.users.${username} = {
-          uid = 1000;
-          isNormalUser = true;
-          extraGroups = [
-            "wheel"
-            "networkmanager"
+          boot-disk.device = "/dev/nvme0n1";
+          boot.initrd.availableKernelModules = [
+            "nvme"
+            "xhci_pci"
+            "thunderbolt"
           ];
-        };
-      }
+
+          programs.zsh.enable = true;
+          users.defaultUserShell = pkgs.zsh;
+
+          # Primary user
+          users.users.${username} = {
+            uid = 1000;
+            isNormalUser = true;
+            extraGroups = [
+              "wheel"
+              "networkmanager"
+            ];
+          };
+        }
+      )
     ];
   };
 
-  # homeConfiguration = self.lib.mkHome {
-  #   inherit system username;
-  #   modules = [ self.homeProfiles.personal ];
-  # };
+  homeConfiguration = self.lib.mkHome {
+    inherit system username;
+    modules = [
+      self.homeProfiles.personal
+      {
+        age.rekey.hostPubkey = userPubkey;
+      }
+    ];
+  };
 }
