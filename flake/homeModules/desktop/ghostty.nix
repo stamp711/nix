c8dd2df848ef21e0fd7bfd3d927d667cc7cb9624
Apricity <stamp1024@gmail.com>
2026-03-12 12:38:14 +0800

    hm: more ghostty config

--- flake/homeModules/desktop/ghostty.nix
+++ flake/homeModules/desktop/ghostty.nix
@@ -5,8 +5,17 @@
     enable = true;
     package = if pkgs.stdenv.isDarwin then pkgs.brewCasks.ghostty else pkgs.ghostty;
     settings = {
+      theme = "Modus Vivendi";
       font-family = "Monaco Nerd Font";
+      font-size = 15;
+      cursor-style-blink = false;
+      shell-integration-features = "cursor,sudo,title,ssh-env,ssh-terminfo,path";
       keybind = [ "global:ctrl+enter=toggle_quick_terminal" ];
+      quick-terminal-position = "top";
+      quick-terminal-size = "100%";
+      quick-terminal-animation-duration = 0;
+      background-opacity = 0.90;
+      background-blur-radius = 5;
     };
   };
 }
