b8391538d836bd4099d71b3e310837e326737f3b
Apricity <stamp1024@gmail.com>
2026-03-12 11:56:02 +0800

    hm: work laptop krbs ticket refresh

--- flake/profiles/work/laptop.nix
+++ flake/profiles/work/laptop.nix
@@ -11,5 +11,19 @@
       my.maintenance.autoUpdate = true;
       my.maintenance.autoClean = true;
       my.ssh.secretConfigFiles = [ ./ssh-config.age ];
+
+      launchd.agents.kinit-renew = {
+        enable = true;
+        config = {
+          ProgramArguments = [
+            "/usr/bin/kinit"
+            "--keychain"
+          ];
+          StartInterval = 14400;
+          RunAtLoad = true;
+          KeepAlive.SuccessfulExit = false;
+          ThrottleInterval = 60;
+        };
+      };
     };
 }
