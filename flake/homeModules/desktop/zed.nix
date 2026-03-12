5c41ae9fa3db6d1a822e169973161e512658b764
Apricity <stamp1024@gmail.com>
2026-03-12 13:58:04 +0800

    hm: zed userSettings

--- flake/homeModules/desktop/zed.nix
+++ flake/homeModules/desktop/zed.nix
@@ -10,7 +10,56 @@
       "nix"
       "xy-zed"
       "cyan-light-theme"
+      "catppuccin"
+      "kanagawa-themes"
+      "modus-themes"
     ];
-    # userSettings = { };
+
+    userSettings = {
+
+      # Essentials
+      telemetry.diagnostics = false;
+      telemetry.metrics = false;
+      cursor_blink = false;
+      session.trust_all_worktrees = true;
+      vim_mode = true;
+
+      # Appearance
+      ui_font_family = "Monaco Nerd Font";
+      buffer_font_family = "Monaco Nerd Font";
+      theme = {
+        mode = "system";
+        light = "Modus Operandi Tinted";
+        dark = "XY-Zed";
+      };
+
+      # LLM
+      edit_predictions.mode = "subtle";
+      agent = {
+        expand_terminal_card = false;
+        enable_feedback = false;
+        single_file_review = true;
+        play_sound_when_agent_done = true;
+      };
+
+      # Language formatters
+      languages = {
+        YAML.formatter.external = {
+          command = "prettier";
+          arguments = [
+            "--stdin-filepath"
+            "{buffer_path}"
+          ];
+        };
+        JSON.formatter.external = {
+          command = "prettier";
+          arguments = [
+            "--stdin-filepath"
+            "{buffer_path}"
+          ];
+        };
+      };
+    };
+
   };
 }
