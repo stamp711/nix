781ab088a0e2a4c660ff511cb73edefcc6798aab
Apricity <stamp1024@gmail.com>
2026-03-12 11:25:28 +0800

    hm: ghostty quick terminal

--- flake/homeModules/desktop/ghostty.nix
+++ flake/homeModules/desktop/ghostty.nix
@@ -2,10 +2,11 @@
 { pkgs, ... }:
 {
   programs.ghostty = {
-    enable = if pkgs.stdenv.isDarwin then false else true;
+    enable = true;
     package = if pkgs.stdenv.isDarwin then pkgs.brewCasks.ghostty else pkgs.ghostty;
     settings = {
       font-family = "Monaco Nerd Font";
+      keybind = [ "global:ctrl+enter=toggle_quick_terminal" ];
     };
   };
 }
