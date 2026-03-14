f7434916e0d5d212635bba69b52406dcb388f1d1
Apricity <stamp1024@gmail.com>
2026-03-15 03:20:31 +0800

    GPD: disable AMD 10bpc kernel patch

--- flake/hosts/personal/gpd/hardware.nix
+++ flake/hosts/personal/gpd/hardware.nix
@@ -9,10 +9,10 @@
   # Force 10bpc to work around Apple Studio Display tile mismatch on Strix Point.
   # One tile reports 12bpc+DSC, the other 10bpc only - forcing 10bpc makes them match.
   # ref: https://gitlab.freedesktop.org/drm/amd/-/issues/4734
-  boot.kernelPatches = [
-    {
-      name = "amdgpu-force-10bpc";
-      patch = ./amdgpu-force-10bpc.patch;
-    }
-  ];
+  # boot.kernelPatches = [
+  #   {
+  #     name = "amdgpu-force-10bpc";
+  #     patch = ./amdgpu-force-10bpc.patch;
+  #   }
+  # ];
 }
