a8598686bce03d3c4f1dbb7ac7e04a8e729b8e70
Apricity <stamp1024@gmail.com>
2026-03-17 11:12:41 +0800

    Point SSH_AUTH_SOCK to 1Password SSH agent on macOS

--- modules/shared/desktop-programs/onepassword.nix
+++ modules/shared/desktop-programs/onepassword.nix
@@ -11,4 +11,21 @@
       # Point SSH_AUTH_SOCK to 1Password SSH agent
       environment.sessionVariables.SSH_AUTH_SOCK = "\${HOME}/.1password/agent.sock";
     };
+
+  # Symlink 1Password SSH agent socket over the system SSH agent socket on macOS
+  flake.darwinModules.desktop-programs =
+    { config, ... }:
+    {
+      launchd.user.agents.onepassword-ssh-auth-sock = {
+        serviceConfig = {
+          Label = "com.1password.SSH_AUTH_SOCK";
+          ProgramArguments = [
+            "/bin/sh"
+            "-c"
+            "/bin/ln -sf /Users/${config.my.primaryUser}/Library/Group\\ Containers/2BUA8C4S2C.com.1password/t/agent.sock $SSH_AUTH_SOCK"
+          ];
+          RunAtLoad = true;
+        };
+      };
+    };
 }
