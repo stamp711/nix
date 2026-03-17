4b2bf3b627eb23b0a18ab528650103ab45b85514
Apricity <stamp1024@gmail.com>
2026-03-17 11:20:43 +0800

    Use watchIdAuth on darwin

--- modules/shared/cli-environment/pam.nix
+++ modules/shared/cli-environment/pam.nix
@@ -8,6 +8,7 @@
     { lib, pkgs, ... }:
     {
       security.pam.services.sudo_local.touchIdAuth = true;
+      security.pam.services.sudo_local.watchIdAuth = true;
       security.pam.services.sudo_local.reattach = true;
       security.pam.services.sudo_local.text = lib.mkAfter ''
         auth sufficient ${pkgs.pam_u2f}/lib/security/pam_u2f.so cue
