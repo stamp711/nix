5f22888007773d13c986bdb2a9c9080128c48b61
Apricity <stamp1024@gmail.com>
2026-04-14 18:00:52 +0800

    socks5h => socks5

--- modules/shared/cli-environment/env.nix
+++ modules/shared/cli-environment/env.nix
@@ -27,11 +27,11 @@
 
       programs.zsh.initContent = ''
         zmodload zsh/net/tcp
-        if ztcp localhost 6153 2>/dev/null; then
+        if ztcp 127.0.0.1 6153 2>/dev/null; then
           ztcp -c
-          export {http,https}_proxy="http://localhost:6152"
-          export {HTTP,HTTPS}_PROXY="http://localhost:6152"
-          export {all_proxy,ALL_PROXY}="socks5h://localhost:6153"
+          export {http,https}_proxy="http://127.0.0.1:6152"
+          export {HTTP,HTTPS}_PROXY="http://127.0.0.1:6152"
+          export {all_proxy,ALL_PROXY}="socks5://127.0.0.1:6153"
           export {no_proxy,NO_PROXY}="localhost,127.0.0.1,::1"
         fi
       '';
