c1e603c21c6307c574be554136082cdf9ab26246
Apricity <stamp1024@gmail.com>
2026-05-07 01:01:10 +0800

    zed: more configurations

--- modules/shared/desktop-programs/zed.nix
+++ modules/shared/desktop-programs/zed.nix
@@ -10,15 +10,32 @@
         enable = true;
         package = if pkgs.stdenv.isDarwin then null else pkgs.zed-editor;
         installRemoteServer = true;
+
         extensions = [
           "wakatime"
           "nix"
-          "xy-zed"
           "cyan-light-theme"
           "catppuccin"
           "kanagawa-themes"
           "modus-themes"
         ];
+
+        mutableUserKeymaps = false;
+        userKeymaps = [
+          {
+            bindings."ctrl-\\" = "workspace::NewCenterTerminal";
+          }
+          {
+            bindings."alt-o" = "editor::SwitchSourceHeader";
+          }
+          {
+            context = "vim_mode == normal || vim_mode == visual";
+            bindings."s" = "vim::PushSneak";
+            bindings."shift-s" = "vim::PushSneakBackward";
+          }
+        ];
+
+        mutableUserSettings = false;
         userSettings = {
           # Essentials
           telemetry.diagnostics = false;
@@ -26,42 +43,80 @@
           cursor_blink = false;
           session.trust_all_worktrees = true;
           vim_mode = true;
+
+          # Layout
+          outline_panel.dock = "left";
+          collaboration_panel.dock = "left";
+          git_panel.dock = "left";
+          project_panel.dock = "left";
+
           # Appearance
           ui_font_family = "Monaco Nerd Font";
           ui_font_size = 14;
           buffer_font_family = "Monaco Nerd Font";
           buffer_font_size = 13;
+          tabs.git_status = true;
           theme = {
             mode = "system";
             light = "Modus Operandi Tinted";
             dark = "Kanagawa Dragon - No Italics";
           };
+
+          # Behaviour
+          format_on_save = "on";
+          terminal.copy_on_select = true;
+
           # LLM
-          edit_predictions.mode = "subtle";
+          edit_predictions = {
+            provider = "copilot";
+            mode = "subtle";
+          };
           agent = {
+            dock = "right";
             expand_terminal_card = false;
             enable_feedback = false;
             single_file_review = true;
             play_sound_when_agent_done = "when_hidden";
           };
-          # Language formatters
-          languages = {
-            YAML.formatter.external = {
-              command = "prettier";
-              arguments = [
-                "--stdin-filepath"
-                "{buffer_path}"
-              ];
-            };
-            JSON.formatter.external = {
-              command = "prettier";
-              arguments = [
-                "--stdin-filepath"
-                "{buffer_path}"
-              ];
-            };
+          agent_servers = {
+            github-copilot-cli.type = "registry";
+            claude-acp.type = "registry";
           };
-          "document_symbols" = "on";
+
+          # Languages
+          languages =
+            let
+              prettierFormatter.external = {
+                command = "prettier";
+                arguments = [
+                  "--stdin-filepath"
+                  "{buffer_path}"
+                ];
+              };
+            in
+            {
+              Nix = {
+                language_servers = [
+                  "nixd"
+                  "nil"
+                ];
+                formatter.external.command = "nixfmt";
+              };
+
+              CMake.formatter.external = {
+                command = "gersemi";
+                arguments = [ "-" ];
+              };
+
+              Python.formatter.language_server.name = "ruff";
+              JSON.formatter = prettierFormatter;
+              YAML.formatter = prettierFormatter;
+            };
+
+          code_lens = "on";
+          document_symbols = "on";
+          diagnostics.inline.enabled = true;
+          inlay_hints.toggle_on_modifiers_press.control = true;
         };
       };
     };
