38169e4ef46a569cc3737852508c7a808fac74b0
Apricity <stamp1024@gmail.com>
2026-05-17 14:19:15 +0800

    darwin: add extra brew casks

---
+++ modules/shared/desktop-programs/extra-macos-apps.nix
@@ -0,0 +1,13 @@
+{
+  flake.darwinModules.desktop-programs = {
+    homebrew.casks = [
+      "bartender"
+      "dash"
+      "flux-app"
+      "iterm2"
+      "raycast"
+      "surge"
+      "zotero"
+    ];
+  };
+}
