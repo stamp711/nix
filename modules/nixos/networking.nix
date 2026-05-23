20c40c202c9c2c2a4ca682f6c1e7b9f3dd7a4d19
Apricity <stamp1024@gmail.com>
2026-05-23 16:06:08 +0800

    networking: persist NetworkManager state (system-connections + /var/lib/NetworkManager)

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -9,5 +9,13 @@
           wifi.powersave = true;
         };
       };
+
+      # NM state must survive @root wipes, secret_key in /var/lib/NetworkManager
+      # encrypts the credentials stored under /etc/NetworkManager/system-connections,
+      # so both must move together.
+      my.persistence.directories = [
+        "/etc/NetworkManager/system-connections"
+        "/var/lib/NetworkManager"
+      ];
     };
 }
