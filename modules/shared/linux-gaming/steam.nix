e63a9a8c774fb03926870ffe37e0b8105e00544a
Apricity <stamp1024@gmail.com>
2026-05-23 22:33:31 +0800

    steam: ship steamos-session-select stub so Switch to Desktop works

--- modules/shared/linux-gaming/steam.nix
+++ modules/shared/linux-gaming/steam.nix
@@ -10,6 +10,13 @@
         extest.enable = true;
         remotePlay.openFirewall = true;
         localNetworkGameTransfers.openFirewall = true;
+        # Big Picture's "Switch to Desktop" exec's this; -shutdown exits
+        # steam cleanly, which ends gamescope, returning to greetd.
+        extraPackages = [
+          (pkgs.writeShellScriptBin "steamos-session-select" ''
+            exec steam -shutdown
+          '')
+        ];
       };
     };
 }
