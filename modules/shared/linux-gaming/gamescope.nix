e5ba6e0093371a24bf44902b36e119e3ba854c5c
Apricity <stamp1024@gmail.com>
2026-05-24 17:58:23 +0800

    linux-gaming: use hardware.graphics.extraPackages{,32} for gamescope-wsi; add mangohud for --mangoapp

--- modules/shared/linux-gaming/gamescope.nix
+++ modules/shared/linux-gaming/gamescope.nix
@@ -8,7 +8,13 @@
         # https://github.com/NixOS/nixpkgs/issues/523200
         capSysNice = false;
       };
-      # WSI Vulkan layer for HDR + frame pacing under gamescope.
-      environment.systemPackages = [ pkgs.gamescope-wsi ];
+      # WSI Vulkan layer for HDR + frame pacing under gamescope; 32-bit
+      # variant needed for older Steam games. Jovian-NixOS pattern.
+      hardware.graphics.extraPackages = [ pkgs.gamescope-wsi ];
+      hardware.graphics.extraPackages32 = [ pkgs.pkgsi686Linux.gamescope-wsi ];
+
+      # mangoapp for `gamescope --mangoapp`; gamescope runs on host so
+      # needs the binary on host PATH (Steam FHS doesn't reach gamescope).
+      environment.systemPackages = [ pkgs.mangohud ];
     };
 }
