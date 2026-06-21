794d81c8418938725b1b4438ad07be288bf341bd
Apricity <stamp1024@gmail.com>
2026-06-21 14:51:42 +0800

    zed: incr font size

--- modules/shared/cli-programs/zed-settings.nix
+++ modules/shared/cli-programs/zed-settings.nix
@@ -77,9 +77,9 @@
 
           # Appearance
           ui_font_family = "Monaco Nerd Font";
-          ui_font_size = 14;
+          ui_font_size = 15;
           buffer_font_family = "Monaco Nerd Font";
-          buffer_font_size = 13;
+          buffer_font_size = 14;
           theme = {
             mode = "system";
             light = "Modus Operandi Tinted";
