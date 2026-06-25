eabad134008a80d32f94733afcc76accd4d4ad53
Apricity <stamp1024@gmail.com>
2026-06-25 21:57:05 +0800

    nix-config: add nix-community binary cache

--- modules/shared/core/nix-config.nix
+++ modules/shared/core/nix-config.nix
@@ -8,10 +8,12 @@ let
     extra-substituters = [
       "https://cache.numtide.com"
       "https://cache.nixos-cuda.org"
+      "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
       "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 in
