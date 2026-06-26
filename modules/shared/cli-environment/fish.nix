4bedd2ae37616e6eee400f3e4716f48a9d52967d
Apricity <stamp1024@gmail.com>
2026-06-27 02:25:47 +0800

    fish: disable programs.man.generateCaches on darwin

--- modules/shared/cli-environment/fish.nix
+++ modules/shared/cli-environment/fish.nix
@@ -1,5 +1,9 @@
 {
-  flake.homeModules.cli-environment = {
-    programs.fish.enable = true;
-  };
+  flake.homeModules.cli-environment =
+    { lib, pkgs, ... }:
+    {
+      programs.fish.enable = true;
+      # fish enables man cache generation, but Darwin has no man package
+      programs.man.generateCaches = lib.mkIf pkgs.stdenv.isDarwin false;
+    };
 }
