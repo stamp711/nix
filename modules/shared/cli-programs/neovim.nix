ea1fe4d3e6bdff557e90ca0d3932cac33ecba69a
Apricity <stamp1024@gmail.com>
2026-06-27 16:14:32 +0800

    neovim: new lvim wrapper

--- modules/shared/cli-programs/neovim.nix
+++ modules/shared/cli-programs/neovim.nix
@@ -1,4 +1,4 @@
-# Neovim: hand-rolled nixvim, baked and enabled as `nvim`.
+# Neovim: hand-rolled nixvim as `nvim`; stock neovim + LazyVim starter as `lvim`.
 { self, inputs, ... }:
 {
   flake.homeModules.cli-programs =
@@ -12,5 +12,26 @@
         nixpkgs.pkgs = pkgs; # our overlaid, allowUnfree pkgs
         imports = [ self.nixvimModules.default ];
       };
+
+      # lvim: stock neovim + the LazyVim starter, isolated from nvim via NVIM_APPNAME
+      home.packages = [
+        (pkgs.writeShellApplication {
+          name = "lvim";
+          runtimeInputs = [
+            pkgs.neovim
+            pkgs.git
+          ];
+          text = ''
+            export NVIM_APPNAME=lazyvim
+            config="''${XDG_CONFIG_HOME:-$HOME/.config}/lazyvim"
+            # fetch the starter on first run
+            if [ ! -e "$config/init.lua" ]; then
+              git clone https://github.com/LazyVim/starter "$config"
+              rm -rf "$config/.git"
+            fi
+            exec nvim "$@"
+          '';
+        })
+      ];
     };
 }
