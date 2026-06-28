29ec553963045456809822f86886f887d4b056e9
Apricity <stamp1024@gmail.com>
2026-06-28 22:44:44 +0800

    nixvim: disable autoFold comment

--- nixvim/editing/fold.nix
+++ nixvim/editing/fold.nix
@@ -20,6 +20,7 @@
         require("which-key").add({ { "z", group = "fold" } })
         -- nr2char(0xF0616) = nf-md-arrow_expand
         require("origami").setup({
+          autoFold = { kinds = { "imports" } }, -- auto-fold imports, not comments
           foldtext = {
             lineCount = { template = vim.fn.nr2char(0xF0616) .. " %d" },
             closingLine = { enabled = true }, -- patched-in option (flake/overlays/origami-closingline.patch)
