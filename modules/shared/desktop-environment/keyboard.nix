2e2b939d5a859f4b82281068edf4a1fa90faddd8
Apricity <stamp1024@gmail.com>
2026-07-01 12:31:51 +0800

    darwin: tap left ctrl as esc

--- modules/shared/desktop-environment/keyboard.nix
+++ modules/shared/desktop-environment/keyboard.nix
@@ -1,10 +1,12 @@
-{
+{ lib, ... }: {
+
   flake.darwinModules.desktop-environment = {
     system.defaults.NSGlobalDomain = {
       KeyRepeat = 1;
       InitialKeyRepeat = 15;
       AppleKeyboardUIMode = 2;
     };
+    homebrew.casks = [ "karabiner-elements" ];
   };
 
   flake.nixosModules.desktop-environment = {
@@ -16,4 +18,38 @@
       };
     };
   };
+
+  flake.homeModules.desktop-environment = { pkgs, ... }: {
+    xdg.configFile."karabiner/karabiner.json" = lib.mkIf pkgs.stdenv.isDarwin {
+      force = true;
+      text = builtins.toJSON {
+        profiles = [
+          {
+            name = "Default";
+            selected = true;
+            virtual_hid_keyboard.keyboard_type_v2 = "ansi";
+            complex_modifications.rules = [
+              {
+                description = "Post escape if left_control is tapped";
+                manipulators = [
+                  {
+                    type = "basic";
+                    from = {
+                      key_code = "left_control";
+                      modifiers.optional = [ "any" ];
+                    };
+                    to = [ { key_code = "left_control"; } ];
+                    to_if_alone = [ { key_code = "escape"; } ];
+                    parameters."basic.to_if_alone_timeout_milliseconds" = 250;
+                  }
+                ];
+              }
+            ];
+          }
+        ];
+      };
+
+    };
+  };
+
 }
