59a2888e65a06c35d11356f489cfab98c413ac8b
Apricity <stamp1024@gmail.com>
2026-07-02 17:26:39 +0800

    treefmt: don't use fixed config

--- modules/shared/cli-programs/treefmt.nix
+++ modules/shared/cli-programs/treefmt.nix
@@ -1,12 +1,9 @@
 # Multi-language code formatter (treefmt wrapped with nixfmt, stylua, prettier, etc.)
-{ self, ... }:
 {
-  flake.homeModules.cli-programs =
-    { pkgs, ... }:
-    {
-      home.packages = [
-        self.formatter.${pkgs.stdenv.hostPlatform.system}
-        pkgs.prettier
-      ];
-    };
+  flake.homeModules.cli-programs = { pkgs, ... }: {
+    home.packages = with pkgs; [
+      treefmt
+      prettier
+    ];
+  };
 }
