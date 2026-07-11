0ff2ffef79690b93c649c932b196f2ff822cfa3c
Apricity <stamp1024@gmail.com>
2026-07-11 21:12:29 +0800

    tailscale: don't accept routes by default

--- modules/nixos/tailscale.nix
+++ modules/nixos/tailscale.nix
@@ -1,13 +1,8 @@
 {
-  flake.nixosModules.tailscale =
-    { lib, ... }:
-    {
-      services.tailscale.enable = true;
-      services.tailscale.useRoutingFeatures = lib.mkDefault "client";
-      services.tailscale.extraSetFlags = [
-        "--accept-routes"
-        "--ssh"
-      ];
-      my.persistence.directories = [ "/var/lib/tailscale" ];
-    };
+  flake.nixosModules.tailscale = { lib, ... }: {
+    services.tailscale.enable = true;
+    services.tailscale.useRoutingFeatures = lib.mkDefault "client";
+    services.tailscale.extraSetFlags = [ "--ssh" ];
+    my.persistence.directories = [ "/var/lib/tailscale" ];
+  };
 }
