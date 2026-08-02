bbf4fcce45f84f740a01257750c1371e942ee027
Apricity <stamp1024@gmail.com>
2026-08-02 15:44:51 +0800

    nuc: pin the multica container pubkeys

--- hosts/personal/nuc/containers/multica.nix
+++ hosts/personal/nuc/containers/multica.nix
@@ -9,6 +9,8 @@
       my.containers = {
         multica = {
           macvlan = "enp4s0";
+          hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH/jMf11vuK96PYpriaiDrMzK84dZLmyrXcOpTKKdnDd";
+          userPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIALLBecHQg9j3ORcEWlEeySjL+ZMMUGBbK3M2VipnTJi";
           hostDirs = [ "${home}/code" ];
           nixosModules = [ self.profiles.nixos.headless ];
           homeModules = [
