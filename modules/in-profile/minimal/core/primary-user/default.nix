dd78ece99604ff8c9e5846d430960b18c5cdbff8
Apricity <stamp1024@gmail.com>
2026-08-05 02:09:50 +0800

    darwin: set the primary user's home

--- modules/in-profile/minimal/core/primary-user/default.nix
+++ modules/in-profile/minimal/core/primary-user/default.nix
@@ -21,6 +21,9 @@
 
   flake.darwinModules.core = { config, ... }: {
     system.primaryUser = config.my.primaryUser;
+    # Modules interpolate this but it defaults to null.
+    # Activation aborts loudly if the account's real one differs.
+    users.users.${config.my.primaryUser}.home = "/Users/${config.my.primaryUser}";
   };
 
   flake.homeModules.core = { config, ... }: {
