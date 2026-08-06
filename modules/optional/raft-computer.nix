701e498a426377d712397660fe847a2b6ada49dc
Apricity <stamp1024@gmail.com>
2026-08-07 01:28:58 +0800

    raft-computer: reap what detaches, with tini as subreaper

--- modules/optional/raft-computer.nix
+++ modules/optional/raft-computer.nix
@@ -11,7 +11,9 @@
       systemd.user.services.raft-computer = {
         Install.WantedBy = [ "default.target" ];
         Service = {
-          ExecStart = "${pkg}/bin/raft-computer __service --os-supervised systemd-user";
+          # tini adopts whatever detaches from an agent, so it keeps hanging under this unit in a process tree.
+          # NOTE: stopping the unit already killed it either way, by cgroup; this is about seeing whose it is.
+          ExecStart = "${pkgs.tini}/bin/tini -s -- ${pkg}/bin/raft-computer __service --os-supervised systemd-user";
           Restart = "always";
           RestartSec = 5;
         };
