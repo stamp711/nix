d24374a927bad4d4eb9fb933f3ce87150fdd4c99
Apricity <stamp1024@gmail.com>
2026-08-10 22:45:37 +0800

    use-build-machine: take the ssh key from the host key we declare

--- modules/optional/use-build-machine.nix
+++ modules/optional/use-build-machine.nix
@@ -1,24 +1,30 @@
 # Offload builds to NUC
-{ self, ... }:
+{ lib, self, ... }:
 let
   hostName = "nuc.boar-char.ts.net";
   publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJXPqL5kmB12FkY38iEo57HrkyadcFxpBvKqWqYRE7zl";
 in
 {
-  flake.nixosModules.use-build-machine = {
-    nix.distributedBuilds = true;
-    nix.settings.builders-use-substitutes = true;
-    nix.buildMachines = [
-      {
-        inherit hostName;
-        sshUser = "stamp";
-        sshKey = "/persist/etc/ssh/ssh_host_ed25519_key";
-        systems = [ "x86_64-linux" ];
-        protocol = "ssh-ng";
-        maxJobs = 8;
-        supportedFeatures = self.nixosConfigurations.NUC.config.nix.settings.system-features;
-      }
-    ];
-    programs.ssh.knownHosts.${hostName} = { inherit publicKey; };
-  };
+  flake.nixosModules.use-build-machine =
+    { config, ... }:
+    {
+      nix.distributedBuilds = true;
+      nix.settings.builders-use-substitutes = true;
+      nix.buildMachines = [
+        {
+          inherit hostName;
+          sshUser = "stamp";
+          sshKey =
+            (lib.findFirst (k: k.type == "ed25519")
+              (throw "use-build-machine: no ed25519 host key to authenticate with")
+              config.services.openssh.hostKeys
+            ).path;
+          systems = [ "x86_64-linux" ];
+          protocol = "ssh-ng";
+          maxJobs = 8;
+          supportedFeatures = self.nixosConfigurations.NUC.config.nix.settings.system-features;
+        }
+      ];
+      programs.ssh.knownHosts.${hostName} = { inherit publicKey; };
+    };
 }
