b2e14b54c84c5a0a5621686bc310bfff0e10c619
Apricity <stamp1024@gmail.com>
2026-09-01 14:17:58 +0800

    overlays: remove zellij wchar patch

--- flake/overlays/modifications/default.nix
+++ flake/overlays/modifications/default.nix
@@ -40,10 +40,6 @@
       patches = (old.patches or [ ]) ++ [
         # Let `attach --config` re-bind keys.
         ./zellij-attach-keybinds.patch
-
-        # Keep a wide character's background on the padding cell when it is erased.
-        # https://github.com/zellij-org/zellij/pull/5180
-        ./zellij-wide-char-padding.patch
       ];
     });
 
