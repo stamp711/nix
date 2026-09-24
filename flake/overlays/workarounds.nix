4ff45824d199728167dc1c343f9ba6f2f0a42f06
Apricity <stamp1024@gmail.com>
2026-09-24 16:12:39 +0800

    overlays: fix nsight_compute shadowing host(1)

--- flake/overlays/workarounds.nix
+++ flake/overlays/workarounds.nix
@@ -25,5 +25,21 @@
         })
       ];
 
+    # 2026-09-24
+    # nixpkgs installs Nsight Compute's host/, target/ and sections/ dirs under bin/,
+    # so bin/host shadows host(1) in every buildEnv. Expose the launchers only.
+    # TODO: remove once nixpkgs moves them out of bin/.
+    cudaPackages = prev.cudaPackages.overrideScope (
+      _: cprev: {
+        nsight_compute =
+          prev.runCommand cprev.nsight_compute.name { inherit (cprev.nsight_compute) meta; }
+            ''
+              [ -d ${cprev.nsight_compute}/bin/host ] || { echo "nsight_compute: bin/host is gone, drop this workaround" >&2; exit 1; }
+              mkdir -p $out/bin
+              ln -s ${cprev.nsight_compute}/bin/ncu{,-ui} $out/bin/
+            '';
+      }
+    );
+
   };
 }
